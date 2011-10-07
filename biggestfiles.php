7b3ad1a347035743a101d1be23340309e1e213cc
Steve Gricci <steve@gricci.org>
2011-10-06 20:42:07 -0400

    Rewrite
    Rewrote the script to use imap_sort, which results in a much quicker
    result.

--- biggestfiles.php
+++ biggestfiles.php
@@ -1,35 +1,28 @@
 #!/usr/local/bin/php
 <?php
-$address  = "email@example.com"; // Email
+$address  = "mail@example.com"; // Email
 $password = "password"; // Password
 $server   = "imap.gmail.com";
 $port     = 993;
 $ssl      = true;
+$results  = 5;
 
 $hostname = sprintf("{%s:%s/imap/ssl/novalidate-cert}[Gmail]/All Mail", $server, $port);
 
 $inbox = imap_open($hostname, $address, $password) OR DIE('Cannot connect to Gmail: '.imap_last_error());
 
-$emails = imap_search($inbox, 'ALL');
+$emails = imap_sort($inbox, SORTSIZE, 1);
 
 if ($emails) {
-	rsort($emails);
 	foreach($emails as $email_number) {
-		$overview = imap_fetch_overview($inbox, $email_number, 0);
-		$mail[$email_number] = $overview;
-		$sizes[str_pad($overview[0]->size, 10, "0", STR_PAD_LEFT)."_".$email_number] = $email_number;
-		echo '#';
+		$header = imap_header($inbox, $email_number, 0);
+		$mail[$email_number] = $header;
+		if (count($mail) == $results) break;
 	}
 }
-echo '\n';
-krsort($sizes);
-$i = 0;
-foreach($sizes as $val)
-{
-	echo $mail[$val][0]->subject.'-'.$mail[$val][0]->size;
-	$i++;
-	if ($i == 2)
-	   break;	
+echo "Largest Emails:\n";
+foreach($mail as $email) {
+	echo $email->subject.'-'.$email->Size."\n";
 }
 imap_close($inbox);
 ?>
